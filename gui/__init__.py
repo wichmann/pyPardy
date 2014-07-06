b27eb7378e516a45ed6fb7375ead35e8747ad03b
Christian Wichmann <cw1981@gmx.de>
2014-07-06 17:03:55 +0200

    Added font to be shipped with pyPardy

--- gui/__init__.py
+++ gui/__init__.py
@@ -6,3 +6,23 @@ Package for all gui components.
 
 @author: Christian Wichmann
 """
+
+
+import os
+from PyQt4 import QtGui
+
+
+def load_shipped_font():
+    """Loads shipped fonts for same look on all plattforms."""
+    FONT_DIRECTOY = './fonts'
+    FONT_NAMES = ('LinuxBiolinum.ttf', )
+    for font in FONT_NAMES:
+        # create path string for each font file
+        # INFO: It seems that QApplication must be initialized before a new
+        # font can be loaded. Without the colon a memory error is thrown by
+        # pyQt! With the colon it seems to be working.
+        font_path = ':' + os.path.join(FONT_DIRECTOY, font)
+        QtGui.QFontDatabase.addApplicationFont(font_path)
+
+
+load_shipped_font()
