f5a35a5b691342211e26fa282f154871b305721a
Christian Wichmann <cw1981@gmx.de>
2014-07-08 15:29:49 +0200

    Added verification for round data files

--- data/round.py
+++ data/round.py
@@ -1,9 +1,12 @@
-#! /usr/bin/env python3
 
 """
 pyPardy
 
 Module for reading and using round data, e.g. questions/answers.
+
+TODO:
+ - Add support for image or audio instead of or additional to questions.
+ - Improve verify process.
 
 @author: Christian Wichmann
 """
@@ -11,10 +14,17 @@ Module for reading and using round data, e.g. questions/answers.
 import glob
 import os
 import json
+import logging
+
+
+logger = logging.getLogger('pyPardy.data')
 
 
 ROUND_DATA_EXTENSION = 'round'
 ROUND_DATA_PATH = 'rounds/'
+
+MAXIMUM_TOPIC_COUNT = 6
+MAXIMUM_QUESTION_COUNT = 7
 
 
 def get_available_round_data():
@@ -51,13 +61,40 @@ def load_round_data_file(filename):
     """
     json_data_file = open(filename)
     data = json.load(json_data_file)
+    if not verify_round_data(data):
+        logger.error('Error in round data file: {}.'.format(filename))
     json_data_file.close()
     return data
 
 
-def verify_round_data():
-    """Verifys the loaded round data from file."""
-    pass
+def verify_round_data(data):
+    """Verifys the loaded round data from file.
+
+    :returns: True, if data is valid round data
+    """
+    try:
+        # check if title exists and if number of topics is valid
+        data['title']
+        if len(data['topics']) > MAXIMUM_TOPIC_COUNT:
+            raise ValueError('To much topics in round data file.')
+        questions_per_topic = len(data['topics'][0]['questions'])
+        for topic in data['topics']:
+            # check if topic title exists and if number of questions in topic
+            # is valid
+            topic['title']
+            if len(topic['questions']) > MAXIMUM_QUESTION_COUNT:
+                raise ValueError('Too much questions in round "{}" in round data file.'.format(topic['title']))
+            # check if all topics have same number of questions
+            if questions_per_topic != len(topic['questions']):
+                raise ValueError('Not all topics have the same number of questions.')
+            # check if all questions have answers :-)
+            for question in topic['questions']:
+                question['question']
+                question['answer']
+    except Exception as e:
+        logger.error(e)
+        return False
+    return True
 
 
 def pprint_round_data(data):
