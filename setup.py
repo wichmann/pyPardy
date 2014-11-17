683958189ba8edb3c72cd41ef3aa7cb10f7dc0ca
Christian Wichmann <cw1981@gmx.de>
2014-11-17 15:54:50 +0100

    * Changed setup.py script.

--- setup.py
+++ setup.py
@@ -19,13 +19,29 @@ executables = [
 
 setup(
     name='pyPardy',
-    version='0.1',
+    #long_description='',
+    keywords='game jeopardy',
+    version='0.2',
     author='Christian Wichmann',
     author_email='christian@freenono.org',
     packages=['data', 'gui'],
     url='',
     license='LICENSE',
     description='Jeopardy(tm) game system',
+    platforms=['any'],
+    classifiers=[
+    'Intended Audience :: End Users/Desktop',
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+    'Operating System :: OS Independent',
+    'Natural Language :: English',
+    'Natural Language :: German',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Topic :: Games/Entertainment',
+    ],
     options=dict(build_exe=buildOptions),
-    executables=executables, requires=['PyQt4']
+    executables=executables, requires=['PyQt4', 'libusb1'],
+    data_files=[('.', 'windows/libusb-1.0.dll')],
 )
